01492cc5485408bcf44813f9fb6a4a2f047ca96b
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-14 03:57:51 -0400

    Se agrego el proyecto mvcWebInfo

---
+++ Informacion/WebInfor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebInfor.MvcApplication" Language="C#" %>
---
+++ Informacion/WebInfor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebInfor.Startup))]
+namespace WebInfor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
